36025c2baa887e54da9b5d1991138201e50eb096
Mihaii2 <mihainegoita9@gmail.com>
2024-05-17 11:08:24 +0300

    Initial commit

---
+++ shift_auto/shift.c
@@ -0,0 +1,22 @@
+#include <8051.h>
+
+int main(int argc, char** argv) {
+  unsigned char x;
+  x = 1;
+  int direction = 1;
+  while(1) {
+      if(direction == 1) {
+        x = x << 1; 
+      }
+      else {
+        x = x >> 1;
+      }
+      if(x == 1 << 7) {
+        direction = 0;
+      }
+      else if(x == 1) {
+        direction = 1;
+      }
+      P1 = ~x;
+  }
+}
---
+++ shift_switch/shift.c
@@ -0,0 +1,18 @@
+#include <8051.h>
+
+int main(int argc, char** argv) {
+  unsigned char x;
+  x = 1;
+  while(1) {
+    if(P2_0 == 1) {
+      x = x << 1;
+      if(x == 0) x = 1;
+      P1 = ~x;
+    }
+    else {
+      x = x >> 1;
+      if(x == 0) x = 1 << 7;
+      P1 = ~x;
+    }
+  }
+}
